0d46623789dddc5d6f7625a478b6b746dc7b078f
Gaonuk <rodrigogaonagonzalez@gmail.com>
2023-11-25 11:56:43 -0300

    ✨ Add correct versions and uptade code

--- packages/contracts/src/APIConsumer.sol
+++ packages/contracts/src/APIConsumer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/dev/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 /**
  * @title The APIConsumer contract
